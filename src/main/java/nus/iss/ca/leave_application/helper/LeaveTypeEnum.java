80edee66d605b6da3c61153b1a2813243fdeeb00
joshualeow <joshualeowzhihao@gmail.com>
2021-12-18 14:23:06 +0800

    LeaveType Update

--- src/main/java/nus/iss/ca/leave_application/helper/LeaveTypeEnum.java
+++ src/main/java/nus/iss/ca/leave_application/helper/LeaveTypeEnum.java
@@ -2,7 +2,16 @@ package nus.iss.ca.leave_application.helper;
 
 /** @Author Fusheng Tan @Version 1.0 */
 public enum LeaveTypeEnum {
-  ANNUAL_LEAVE,
-  MEDICAL_LEAVE,
-  COMPENSATION_LEAVE
-}
+	  ANNUAL_LEAVE("Annual Leave", 1.0),
+	  MEDICAL_LEAVE("Medical Leave", 1.0),
+	  COMPENSATION_LEAVE("Compensation Leave", 1.0),
+	  HALF_COMPENSATION_LEAVE("Half-Day Compensation Leave", 0.5);
+	  
+	  private Double duration;
+	  private final String displayValue;
+	  
+		private LeaveTypeEnum(String displayValue, Double duration){
+			this.displayValue = displayValue;
+			this.duration = duration;
+		}
+	}
