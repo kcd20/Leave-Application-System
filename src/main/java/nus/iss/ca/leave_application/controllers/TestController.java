d2460a13cc7330d0551e33ff9ec0e4ebde5208a3
joshualeow <joshualeowzhihao@gmail.com>
2021-12-23 17:22:49 +0800

    View Templates

--- src/main/java/nus/iss/ca/leave_application/controllers/TestController.java
+++ src/main/java/nus/iss/ca/leave_application/controllers/TestController.java
@@ -3,6 +3,9 @@ package nus.iss.ca.leave_application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import nus.iss.ca.leave_application.model.Application;
 
 /**
  * @Author Fusheng Tan
@@ -11,13 +14,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 	
-	@RequestMapping("/report")
+	@RequestMapping(value="/report", method = RequestMethod.GET)
 	public String showReport(Model model) {
+		Application app = new Application();
 		return "report";
 	}
 	
-	@RequestMapping("/compensation")
+	@RequestMapping(value="/annualreport", method = RequestMethod.GET)
+	public String showAnnualReport(Model model) {
+		return "annualreport";
+	}
+	
+	@RequestMapping(value="/medicalreport", method = RequestMethod.GET)
+	public String showMedicalReport(Model model) {
+		return "medicalreport";
+	}
+	
+	@RequestMapping(value="/compensationreport", method = RequestMethod.GET)
+	public String showCompensationReport(Model model) {
+		return "compensationreport";
+	}
+	
+	
+	@RequestMapping(value="/compensation", method = RequestMethod.GET)
 	public String showPeriod(Model model) {
 		return "compensation";
 	}
 }
+
